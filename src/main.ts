960b269f9cd4c9a97b038a4dd5f7b4c6a416341e
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-10-01 09:30:26 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,9 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
+    cors: true,
   });
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
