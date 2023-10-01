2dfff399305d16900525da4063b9a794a58f549e
IbrahimGaber322 <ibrahimseda322@gmail.com>
2023-10-01 10:42:39 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ async function bootstrap() {
     bodyParser: true,
   });
   app.enableCors({
-    origin: ['*', FRONTEND_URL],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: [FRONTEND_URL, 'http://localhost:3000'],
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
